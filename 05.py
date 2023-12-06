8d99c68b23ea42e7059bfdad80e26082ebea5563
Nina Høegh-Larsen <ninius@online.no>
2023-12-06 01:03:29 +0100

    Day 5 - All done! Never touching that again!

--- 05.py
+++ 05.py
@@ -65,30 +65,29 @@ def check(mapp, value):
 def checkRange(mapp, start, end):
 
     ranges = []
-    to = [[start, end]]
+    extra = [[start, end]]
     i = 0
 
-    while i < len(to):
+    while i < len(extra):
 
-        start = to[i][0]
-        end = to[i][1]
+        start = extra[i][0]
+        end = extra[i][1]
 
         for destination, source, length in mapp:
 
             if start < end < source or source+length < start < end:
                 continue
 
-            elif source <= start <= end <= source+length:
+            elif source <= start <= end < source+length:
 
                 begin   = start + (destination-source)
                 stop    = end + (destination-source)
-
-                start = end
+                
                 ranges.append([begin, stop])
 
                 return ranges
 
-            elif start < source <= end <= source+length:
+            elif start < source <= end < source+length:
                 
                 begin = destination
                 stop = end + (destination-source)
@@ -97,29 +96,27 @@ def checkRange(mapp, start, end):
 
                 end = source-1
 
-            elif source <= start <= source+length < end:
+            elif source <= start < source+length < end:
             
                 begin = start + (destination-source)
-                stop = source+length + (destination-source)
+                stop = source+length-1 + (destination-source)
 
                 ranges.append([begin, stop])
-            
-                start = source+length+1
+
+                start = source+length
             
             elif start < source < source+length < end:
-
-                #print(f"hit! start: {start} source: {source} len: {source+length} end: {end}")
             
-                ranges.append([destination, destination+length])
+                ranges.append([destination, destination+length-1])
 
                 start1 = start
                 end1 = source-1
 
-                start2 = source+length+1
+                start2 = source+length
                 end2 = end
 
-                to.append([start1, end1])
-                to.append([start2, end2])
+                extra.append([start1, end1])
+                extra.append([start2, end2])
 
         ranges.append([start, end])
 
@@ -136,7 +133,7 @@ def expand(seeds):
         num = seeds[0+x]
         rng = seeds[1+x]
 
-        new.append([num, num+rng])
+        new.append([num, num+rng-1])
 
     return new
 
@@ -175,68 +172,38 @@ def parse_again(input):
     locs = []
     
     for start, end in seeds:
-        #print(f"checking: {start} {end}")
         soil = checkRange(soilmap, start, end)
         soils += soil
 
-    #print(soils)
-
-    print("fer")
-
     for start, end in soils:
-        #print(f"checking: {start} {end}")
-        #print(fermap)
         fer = checkRange(fermap, start, end)
         fers += fer
 
-    print("water")
-
     for start, end in fers:
-        #print(f"checking: {start} {end}")
-        #print(watermap)
         water = checkRange(watermap, start, end)
         waters += water
 
-    print("light")
-
     for start, end in waters:
-        #print(f"checking: {start} {end}")
-        #print(lightmap)
         light = checkRange(lightmap, start, end)
         lights += light
 
-    print("temp")
-
     for start, end in lights:
-        #print(f"checking: {start} {end}")
-        #print(tempmap)
         temp = checkRange(tempmap, start, end)
         temps += temp
-        #print(temps)
-
-    print("hums")
 
     for start, end in temps:
-        #print(f"checking: {start} {end}")
         hum = checkRange(hummap, start, end)
         hums += hum
 
-    print("locs")
-
     for start, end in hums:
-        #print(f"checking: {start} {end}")
         loc = checkRange(locmap, start, end)
         locs += loc
-   
-    #print(locs)
-    print("final")
 
     smallest = math.inf
 
     for start, end in locs:
         if start < smallest:
             smallest = start
-            print(smallest)
 
     return smallest
 
@@ -248,8 +215,6 @@ def part1(input):
 def part2(input):
     
     small = parse_again(input)
-    #small = min(locations)
-
     print("Part 2: {}".format(small))    
 
 filename = "input/05.txt"
